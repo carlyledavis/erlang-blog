56a0e060af54fccb441316ab90cd6e02c529f0c8
Carlyle S. Davis <carlyle.davis@gmail.com>
2017-01-26 20:47:38 -0500

    initial commit.

---
+++ lib/blog/webplug.ex
@@ -0,0 +1,23 @@
+defmodule Blog.Webplug do
+    use Plug.Router
+
+    plug :match
+    plug :dispatch
+
+    def init(options) do
+      options
+    end
+
+    def start_link do
+      Plug.Adapters.Cowboy.http(Plugger.Router, [])
+    end
+
+    get "/" do
+      IO.puts "This is asking for something."
+      send_resp( conn, 200, "ok" )
+    end
+
+    get "/:resource" do
+      send_resp( conn, 200, resource )
+    end
+end
---
+++ mix.exs
@@ -0,0 +1,22 @@
+defmodule Blog.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :blog,
+     version: "0.1.0",
+     elixir: "~> 1.3",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps()]
+  end
+
+  def application do
+    [applications: [:logger, :cowboy, :poison, :plug]]
+  end
+
+  defp deps do
+    [{:cowboy, "~> 1.0.0"},
+    {:plug, "~> 1.0"},
+    {:poison, "~> 3.0"}]
+  end
+end
